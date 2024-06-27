3af76f38ac9c383644449280b0137304a22c4d64
Gabz047 <gabe1612mail@gmail.com>
2024-06-27 11:55:17 -0300

    Fix: Imagens estao sendo puxadas do banco #5

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,20 @@
   <main>
     <section class="container-presentation">
       <div class="box-texts-presentation">
-        <div>
+        <div class="box-left-text">
           <h1 class="tittle-presentation">HORSEBACK</h1>
           <h2 class="subtittle-presentation">Explore UK from the saddle</h2>
         </div>
 
-        <div>
-          <h3>FOLLOW YOUR DREAM</h3>
+        <div class="box-right-text">
+          <h3>FOLLOW</h3>
+          <h3>YOUR</h3>
+          <h3>DREAM</h3>
         </div>
+      </div>
+
+      <div class="box-button-book">
+        <button>Book Now</button>
       </div>
     </section>
   </main>
@@ -29,40 +35,79 @@ main {
   background-image: url('./assets/media/cavalo.jpg');
   background-size: cover;
   background-position: top;
+  display: flex;
+  width: 100vw;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 
 .box-texts-presentation {
-  border: 2px solid black;
   position: relative;
+  width: 100%;
   z-index: 0;
   height: 50vh;
-  top: 10rem;
+  top: 9rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
-
+  border: 2px solid black;
 }
 
-.box-texts-presentation div {
-  margin-left: 5rem;
+.box-left-text {
+  margin-left: 3rem;
 }
 
 .tittle-presentation {
   color: var(--vt-c-white);
   font-weight: 700;
   letter-spacing: 0.15rem;
-  font-size: 4.5rem;
+  font-size: 4rem;
 }
 
 .tittle-presentation::after {
   content: ' RIDING';
-  letter-spacing: 0.15rem;
+  letter-spacing: 0.1rem;
 }
 
 .subtittle-presentation {
   color: var(--vt-c-white);
   font-weight: 400;
-  letter-spacing: 0.15rem;
-  font-size: 2.5rem;
+  letter-spacing: 0.1rem;
+  font-size: 2rem;
+}
+
+.box-right-text {
+  color: var(--vt-c-white);
+  height: 100%;
+  max-width: 17px;
+  display: flex;
+  font-size: 0.7rem;
+  word-break: break-all;
+  margin: 10rem 2rem 0 0;
+  letter-spacing: 1rem;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.box-right-text h3 {
+  font-weight: 600;
+  text-align: center;
+}
+
+.box-button-book {
+  width: 50%;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.box-button-book button {
+  border: 2px solid var(--vt-c-white);
+  background-color: transparent;
+  border-radius: 50px;
+  width: 60%;
+  height: 80%;
+  font-size: 1.5rem;
+  color: var(--vt-c-white);
 }
 </style>
